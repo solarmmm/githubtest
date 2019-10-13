1aeca7439ad097043b71943c1d4fb83d6594b575
Hello <Hello@DESKTOP-4EJGF6P>
2019-10-13 15:33:20 +0900

    create clone

--- githubtest/src/githubtest/Main.java
+++ githubtest/src/githubtest/Main.java
@@ -4,8 +4,5 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		
-		System.out.println("totay is sunday");
-		
-		System.out.println("first day");
 	}
 }
